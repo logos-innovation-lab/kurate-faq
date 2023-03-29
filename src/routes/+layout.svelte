b2e76b5ff36abc614261fb61177f6c0cdbb80a8a
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-03-29 12:52:31 -0400

    style: article page - Add styles to article detail page - Remove header from root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,7 @@
-<script>
+<script lang="ts">
 	import './styles.css';
 	import '@fontsource/source-sans-pro';
-	import Container from '$lib/components/container.svelte';
-	import Divider from '$lib/components/divider.svelte';
-	import Header from '$lib/components/header.svelte';
-	import Search from '$lib/components/search.svelte';
 </script>
-
-<Container>
-	<Header title="FAQ" />
-	<Search />
-</Container>
-<Divider />
 
 <main>
 	<slot />
