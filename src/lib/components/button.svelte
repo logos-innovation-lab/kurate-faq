35be43d9fa744db66fd4d33b2d6aa9da5cbe9cb8
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-04-04 17:17:38 -0400

    feat: add footer content and styles

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -3,18 +3,12 @@
 
 	let cls: string | undefined = undefined;
 	export { cls as class };
-	export let variant: 'secondary' | 'primary' | 'noBorder' = 'secondary';
 	export let icon: ComponentConstructor<IconProps> | undefined = undefined;
 	export let label: string | undefined = undefined;
 	export let disabled: boolean | undefined = undefined;
 </script>
 
-<button
-	type="button"
-	{disabled}
-	class={`root ${variant} ${!label ? 'icon-only' : ''} ${cls}`}
-	on:click
->
+<button type="button" {disabled} class={`root ${!label ? 'icon-only' : ''} ${cls}`} on:click>
 	{#if icon !== undefined}
 		<div class="wrapper">
 			<svelte:component this={icon} />
@@ -35,7 +29,7 @@
 		box-sizing: border-box;
 		border-radius: 50px;
 		cursor: pointer;
-		display: flex;
+		display: inline-flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: center;
@@ -44,27 +38,10 @@
 		font-size: var(--font-size-normal);
 		transition: border-color 0.2s, background-color 0.2s, color 0.2s;
 		white-space: nowrap;
-
-		&:disabled {
-			cursor: not-allowed;
-		}
-	}
-	.icon-only {
-		width: 44px;
-	}
-
-	.icon-only .wrapper {
-		margin-right: 0px;
-	}
-	.wrapper {
-		width: 20px;
-		height: 20px;
-		margin-right: var(--spacing-6);
-	}
-	.primary {
 		color: var(--color-body-bg);
-		border-color: var(--color-body-text);
+		border-color: var(--grey-400);
 		background-color: var(--color-body-text);
+		width: fit-content;
 
 		& :global(svg) {
 			fill: var(--color-body-bg);
@@ -74,84 +51,18 @@
 			background-color: var(--grey-200);
 			border-color: var(--grey-200);
 			color: var(--color-body-bg);
+			cursor: not-allowed;
 		}
 
 		&:active:not(:disabled),
 		&:hover:not(:disabled) {
-			border-color: var(--color-black);
 			background-color: var(--color-black);
-			transition: border-color 0.2s, background-color 0.2s;
-		}
-
-		@media (prefers-color-scheme: dark) {
-			&:disabled {
-				background-color: var(--grey-200);
-				color: var(--color-body-bg);
-
-				& :global(svg) {
-					fill: var(--color-body-bg);
-				}
-			}
-
-			&:active:not(:disabled),
-			&:hover:not(:disabled) {
-				transition: border-color 0.2s, background-color 0.2s;
-			}
+			transition: background-color 0.2s;
 		}
 	}
-	.secondary {
-		background-color: transparent;
-		border-color: var(--grey-200);
-		color: var(--color-body-text);
-
-		& :global(svg) {
-			fill: var(--color-body-text);
-		}
-
-		&:disabled {
-			color: var(--grey-200);
-
-			& :global(svg) {
-				fill: var(--grey-200);
-			}
-		}
-
-		&:active:not(:disabled),
-		&:hover:not(:disabled) {
-			background-color: var(--grey-150);
-			transition: border-color 0.2s, background-color 0.2s;
-		}
-
-		@media (prefers-color-scheme: dark) {
-			background-color: var(--color-body-bg);
-			border-color: var(--color-body-text);
-			color: var(--color-body-text);
-
-			& :global(svg) {
-				fill: var(--color-body-text);
-			}
-
-			&:disabled {
-				background-color: var(--color-body-bg);
-				color: var(--grey-500);
-
-				& :global(svg) {
-					fill: var(--grey-500);
-				}
-			}
-
-			&:active:not(:disabled),
-			&:hover:not(:disabled) {
-				transition: border-color 0.2s, background-color 0.2s;
-			}
-		}
-	}
-
-	.noBorder {
-		border-color: transparent;
-		background-color: transparent;
-		padding: 0;
-		height: 20px;
+	.wrapper {
 		width: 20px;
+		height: 20px;
+		margin-right: var(--spacing-6);
 	}
 </style>
